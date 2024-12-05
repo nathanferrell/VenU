a425dc267bba5dff375d35834b2a265d330fa524
lailahamdan1116 <94996494+lailahamdan1116@users.noreply.github.com>
2024-12-04 20:35:52 -0600

    Update RecentConcerts.js for navigation

--- components/RecentConcerts.js
+++ components/RecentConcerts.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import Card from './Card';
 import { commonStyles } from '../styles';
 const upcomingconcerts = require('../data/upcomingconcerts.json');
@@ -7,7 +8,8 @@ const concerts = require('../data/concerts.json');
 const venues = require('../data/venues.json');
 const artists = require('../data/artists.json');
 
-const RecentConcerts = () => (
+const RecentConcerts = () => {
+const navigation = useNavigation();
   <View>
     <Text style={commonStyles.header}>Recent Concerts</Text>
     <FlatList
@@ -18,18 +20,21 @@ const RecentConcerts = () => (
         const venue = venues.find(v => v.id === item.venueId);
         const concertArtists = item.artistIds.map(id => artists.find(a => a.id === id));
         return (
+         <TouchableOpacity onPress={() => navigation.navigate('ConcertDetail', { concertId: item.id })}>
           <Card>
             <Text style={commonStyles.title}>{item.name}</Text>
             <Text style={commonStyles.text}>{item.date}</Text>
             <Text style={commonStyles.text}>{venues.name},{venues.location}</Text>
             {concertArtists.map(artist => (
-              <Text key={artist.id} style={commonStyles.text}>{artist.genre}</Text>
+              <Text key={artist.id} style={commonStyles.text}>{artist.genre}</Text>     
             ))}
           </Card>
+          </TouchableOpacity>
         );
       }}
     />
   </View>
 );
+};
 
 export default RecentConcerts;
